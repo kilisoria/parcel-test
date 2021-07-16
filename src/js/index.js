158726cf557c32280a78eeda1b7a8bdd386f9624
Cristian Soria <cristiansoria@ip-192-168-0-69.ec2.internal>
2021-07-16 14:26:35 -0300

    initial commit

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+function getInfo (name, year = 2018, color = 'blue') {
+    console.log(name, year, color);
+  }
+  
+  getInfo('Chevy', 1957, 'Green');
+  getInfo('Benz', 1975);
+  getInfo('Honda');
